a66a6e64abf20300c3d3f29284a3ba06d1baf32b
seb1989-web <56884087+seb1989-web@users.noreply.github.com>
2019-11-03 18:43:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stiva = new Stack(new Stack<string>());
+            stiva.Push("costel");
+            stiva.Push("ion");
+            stiva.Push("vasile");
+            stiva.Print();
+            Console.WriteLine();
+            stiva.Pop();
+            stiva.Print();
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    class Stack
+    {
+        Stack<string> stiva = new Stack<string>();
+
+        public Stack(Stack<string> stiva )
+        {
+            this.stiva = stiva;
+        }
+
+        public void Push(string value)
+        {
+            stiva.Push(value);
+        }
+
+        public void Pop()
+        {
+            stiva.Pop();
+        }
+
+        public void Print()
+        {
+            foreach (var item in stiva)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
